80cda49505988a23e4f6540d00ae9923db0e57bb
chan <thechanmoon@gmail.com>
2020-05-07 09:48:58 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -5,28 +5,6 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Counter />
-      </div>
-    );
-  }
-}
-
-class Header extends Component {
-  render() {
-    return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-    );
-  }
-}
-
-class Counter extends Component {
   state = { count: 0 };
 
   increment = () => {
@@ -37,6 +15,53 @@ class Counter extends Component {
     this.setState(prevState => ({ count: prevState.count - 1 }));
   };
 
+  render() {
+    return (
+      <div className="App">
+        <Header 
+        state = {this.state}
+        />
+        <Counter 
+        state = {this.state}
+        increment = {this.increment}
+        decrement = {this.decrement}
+        />
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  
+  
+  renderDescription = () => {
+    const remainder = this.props.state.count % 5;
+    const upToNext = 5 - remainder;
+    return `The current count is less than ${this.props.state.count + upToNext}`;
+  };
+
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">{this.renderDescription()}</h1>
+         {/* <h3>{this.renderDescription()}</h3> */}
+      </header>
+    );
+  }
+}
+
+class Counter extends Component {
+  // state = { count: 0 };
+
+  // increment = () => {
+  //   this.setState(prevState => ({ count: prevState.count + 1 }));
+  // };
+
+  // decrement = () => {
+  //   this.setState(prevState => ({ count: prevState.count - 1 }));
+  // };
+
   renderDescription = () => {
     const remainder = this.state.count % 5;
     const upToNext = 5 - remainder;
@@ -46,10 +71,10 @@ class Counter extends Component {
   render() {
     return (
       <div className="Counter">
-        <h1>{this.state.count}</h1>
-        <button onClick={this.decrement}> - </button>
-        <button onClick={this.increment}> + </button>
-        <h3>{this.renderDescription()}</h3>
+        <h1>{this.props.state.count}</h1>
+        <button onClick={this.props.decrement}> - </button>
+        <button onClick={this.props.increment}> + </button>
+        {/* <h3>{this.renderDescription()}</h3> */}
       </div>
     );
   }
